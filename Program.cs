ff4da15545b60f4fffff3cd3612b6f66f21c68ed
oldma <oldma@OLDMAN>
2023-02-03 11:51:33 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace StarPattern
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int row = 1; row <= 6; row++)
+            {
+                for (int col =1;col <= row; ++col)
+                {
+                    Console.Write("*");
+
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Break");
+            for (int row = 6; row >= 1; -- row)
+            {
+                for (int col = 1; col <= row; ++col)
+                {
+                    Console.Write("*");
+
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Break");
+
+            for (int row = 1; row <= 6; ++row)
+            {
+                for (int col = 1; col <= row; ++col)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+
+    }
+}
